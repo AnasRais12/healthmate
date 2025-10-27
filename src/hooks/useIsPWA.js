5b3e105def849548b27c40474b0399c9e60b0260
Anas Baig <165804278+AnasRais12@users.noreply.github.com>
2025-10-27 16:01:09 +0500

    add isPWA file

---
+++ src/hooks/useIsPWA.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+export default function useIsPWA() {
+  const [isPWA, setIsPWA] = useState(false);
+
+  useEffect(() => {
+    const isStandalone =
+      window.matchMedia("(display-mode: standalone)").matches ||
+      window.navigator.standalone;
+    setIsPWA(isStandalone);
+  }, []);
+
+  return isPWA;
+}
