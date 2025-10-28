b26b8f300f65a2b7b26b28b146c1f7595accbb8e
Anas Baig <165804278+AnasRais12@users.noreply.github.com>
2025-10-28 20:33:04 +0500

    Refactor page component to handle user session

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,51 @@
 "use client"
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 import Home from '@/components/userComponent/Home'
-import { useReduxState } from '@/hooks/useAppUtils'
+import { useAppUtils, useReduxState } from '@/hooks/useAppUtils'
 import Dashboard from '@/components/userComponent/Dashboard'
+import { useSession } from 'next-auth/react'
+import { Box, CircularProgress } from '@mui/material'
+import { googleUser } from '@/service/authService'
 const page = () => {
+  const session = useSession();
+  const [loading, setLoading] = useState(false);
+  const { dispatch, router } = useAppUtils();
   const { userInfo } = useReduxState()
+  useEffect(() => {
+    const fetchUser = async () => {
+
+      if (session?.status === 'loading') {
+        setLoading(true);
+      }
+      else if (session?.status === 'authenticated' || session?.status === 'unauthenticated') {
+        setLoading(false);
+      }
+
+      if (session?.data?.user) {
+        setLoading(true)
+        try {
+          await dispatch(googleUser({ email: session?.data?.user?.email }))
+
+        } finally {
+          setLoading(false)
+        }
+
+      }
+    }
+    if (!userInfo?._id)
+      fetchUser()
+
+  }, [session, session?.data?.user])
+
+  if (loading) return <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: "center", height: "100vh" }}><CircularProgress /></Box>;
+
+
+
   return (
     <>
 
-      {userInfo?._id ?  <Dashboard /> : <Home />}
- 
+      {userInfo?._id ? router.push('/dashboard') : <Home />}
+
     </>
   )
 }
